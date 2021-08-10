ea5f2303cfd52d5394cb5c0d41db3771ac589144
Pratyush <pratyushshukla9333@gmail.com>
2021-08-10 14:28:08 +0530

    Step 4 : JUNIT Best Practices

--- src/test/java/com/in28minutes/juint/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/juint/helper/StringHelperTest.java
@@ -7,16 +7,24 @@ import org.junit.Test;
 import com.in28minutes.junit.helper.StringHelper;
 
 public class StringHelperTest {
-
+	StringHelper helper = new StringHelper() ;
 	@Test
-	public void test() {
-		StringHelper helper = new StringHelper() ;
+	public void TestTruncateAInFirst2Positions_Ainfirst2Positions() {
+		// AACD => CD   ACD => CD   CDEF => CDEF   CDAA => CDAA
+		
 		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
 		assertEquals("CD",helper.truncateAInFirst2Positions("ACD"));
-		assertEquals("CDAA",helper.truncateAInFirst2Positions("CDAA"));
+	
+		//assertEquals("CDAA",helper.truncateAInFirst2Positions("CDAA"));
 
 		// Expected, Actual
-		// AACD => CD   ACD => CD   CDEF => CDEF   CDAA => CDAA
+		
+	}
+	
+	@Test
+	public void TestTruncateAInFirst2Positions_AinBetween() {
+		assertEquals("CCAACD",helper.truncateAInFirst2Positions("CCAACD"));
+
 	}
 	
 
